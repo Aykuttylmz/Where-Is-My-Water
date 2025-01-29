48bb932e3bf8c2aec407ce607a452d47248722fd
Aykut Türkyılmaz <120272313+Aykuttylmz@users.noreply.github.com>
2025-01-29 14:49:46 +0300

    first commit

---
+++ WhereIsMyWater/WIMWHomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  WIMWHomeVC.swift
+//  WhereIsMyWater
+//
+//  Created by Aykut Türkyılmaz on 29.01.2025.
+//
+
+import UIKit
+
+class WIMWHomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+        
+    }
+    
+
+
+}
